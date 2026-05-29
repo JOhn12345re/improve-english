0b2295df03a690b6e723b7e8f431c8d6299529bb
JOhn12345re <shenoudata@cy-tech.fr>
2026-05-29 23:33:05 +0200

    auto: sync changes

--- apps/api/src/modules/ingestion/ingestion.module.ts
+++ apps/api/src/modules/ingestion/ingestion.module.ts
@@ -2,10 +2,20 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from '../../common/prisma/prisma.module';
 import { VoaModule } from './voa/voa.module';
+import { ClassifierModule } from './classifier/classifier.module';
+import { LessonGeneratorModule } from './lesson-generator/lesson-generator.module';
+import { PdfModule } from './pdf/pdf.module';
 import { IngestionController } from './ingestion.controller';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), PrismaModule, VoaModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    PrismaModule,
+    VoaModule,
+    ClassifierModule,
+    LessonGeneratorModule,
+    PdfModule,
+  ],
   controllers: [IngestionController],
 })
 export class IngestionModule {}
