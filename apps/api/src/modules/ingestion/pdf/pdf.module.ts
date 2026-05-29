4c41e4c66f7a3245ae8c1c96a4ad9bd687b6dcfc
JOhn12345re <shenoudata@cy-tech.fr>
2026-05-29 23:32:49 +0200

    auto: sync changes

---
+++ apps/api/src/modules/ingestion/pdf/pdf.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { PrismaModule } from '../../../common/prisma/prisma.module';
+import { PdfExtractorService } from './pdf-extractor.service';
+import { ArchiveOrgFetcherService } from './archive-org-fetcher.service';
+import { GutenbergFetcherService } from './gutenberg-fetcher.service';
+import { PdfIngesterService } from './pdf-ingester.service';
+
+@Module({
+  imports: [PrismaModule],
+  providers: [
+    PdfExtractorService,
+    ArchiveOrgFetcherService,
+    GutenbergFetcherService,
+    PdfIngesterService,
+  ],
+  exports: [PdfIngesterService],
+})
+export class PdfModule {}
