99b4a96f16dc49f497f09c40b07977093d7454aa
JOhn12345re <shenoudata@cy-tech.fr>
2026-05-29 23:05:04 +0200

    auto: sync changes

---
+++ apps/api/src/modules/ingestion/ingestion.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
+import { VoaModule } from './voa/voa.module';
+import { IngestionController } from './ingestion.controller';
+
+@Module({
+  imports: [ScheduleModule.forRoot(), VoaModule],
+  controllers: [IngestionController],
+})
+export class IngestionModule {}
